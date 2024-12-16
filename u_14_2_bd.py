e36a0ca0b7756335e772a7f67415166ed53b9aea
IliaMuza <vasilijcapaev665@gmail.com>
2024-12-16 18:03:19 +0300

    DZ_2

--- u_14_2_bd.py
+++ u_14_2_bd.py
@@ -24,5 +24,17 @@ users = cursor.fetchall()
 for user in users:
     print(f"Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}")
 
+cursor.execute("DELETE FROM Users WHERE id = 6")
+cursor.execute("SELECT COUNT(*) FROM Users")
+total_users = cursor.fetchone()[0]
+
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balances = cursor.fetchone()[0]
+
+cursor.execute("SELECT AVG(balance) FROM Users")
+avg_balance = cursor.fetchone()[0]
+
+print(all_balances/total_users)
+
 connection.commit()
 connection.close()
